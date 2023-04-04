defd2d59ff62ee15e3742bb5708eb7fdbfcdc2cd
Cátia Teixeira <catia.rds.teixeira@gmail.com>
2023-04-04 19:39:45 +0100

    Added product transformation v2, updated DDL
    Signed-off-by: Cátia Teixeira <catia.rds.teixeira@gmail.com>

--- DDL/DDL_v2.sql
+++ DDL/DDL_v2.sql
@@ -1,4 +1,4 @@
-CREATE TABLE dimensional_model.customer
+CREATE TABLE IF NOT EXISTS dimensional_model.customer
 (
   customer_id BIGINT PRIMARY KEY
 , first_name VARCHAR(11)
@@ -8,5 +8,19 @@ CREATE TABLE dimensional_model.customer
 , longitude NUMERIC(20, 8)
 , segment_id DOUBLE PRECISION
 , segment VARCHAR(11)
+)
+;
+
+CREATE TABLE IF NOT EXISTS dimensional_model.product
+(
+  product_id BIGINT PRIMARY KEY
+, name VARCHAR(45)
+, image VARCHAR(150)
+, price BIGINT
+, status BIGINT
+, category_id BIGINT
+, category_name VARCHAR(20)
+, department_id BIGINT
+, department_name VARCHAR(20)
 )
 ;
