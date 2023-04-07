03df381c9e3004752bb14637e6c3d2b653267311
Cátia Teixeira <catia.rds.teixeira@gmail.com>
2023-04-07 16:04:27 +0100

    Added order_items fact and correct datatypes
    Added order_items tables.
    Corrected data type for double precision for all price related variables
    (precision 5) and for latitude and longitue (precision 15).
    
    Signed-off-by: Cátia Teixeira <catia.rds.teixeira@gmail.com>

--- DDL/DDL_v2.sql
+++ DDL/DDL_v2.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS dimensional_model.customer
 , first_name VARCHAR(11)
 , last_name VARCHAR(12)
 , zipcode NUMERIC(8, 1)
-, latitude NUMERIC(20, 8)
-, longitude NUMERIC(20, 8)
-, segment_id DOUBLE PRECISION
+, latitude double precision
+, longitude double precision
+, segment_id BIGINT
 , segment VARCHAR(11)
 )
 ;
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS dimensional_model.product
   product_id BIGINT PRIMARY KEY
 , name VARCHAR(45)
 , image VARCHAR(150)
-, price BIGINT
+, price double precision
 , status BIGINT
 , category_id BIGINT
 , category_name VARCHAR(20)
@@ -52,5 +52,19 @@ CREATE TABLE IF NOT EXISTS dimensional_model.time
 , month DOUBLE PRECISION
 , quarter DOUBLE PRECISION
 , year DOUBLE PRECISION
+)
+;
+
+CREATE TABLE IF NOT EXISTS dimensional_model.order_items
+(
+  product_id BIGINT
+, order_time_id BIGINT
+, discount double precision
+, discount_rate double precision
+, product_price double precision
+, profit_ratio double precision
+, quantity BIGINT
+, sales double precision
+, order_item_total double precision
 )
 ;
